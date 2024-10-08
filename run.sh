9df99a4a0e13451b13832dadfdb23b72bb65b3de
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-10-08 14:17:10 -0400

    Added run script

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+tnn-miner-cpu --spectre --daemon-address 10.0.0.32 --port 5555 --wallet spectre:qqcfuf9ehqfl90qulkyhn0mn9udrptqrwtzsv64r0tc6eshcymg4w9tmrn67z --threads 8 --worker-name pixelN --dev-fee 1.0
